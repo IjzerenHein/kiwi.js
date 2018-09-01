5b880022d3f6521a49226f2aa4a743c836ff5ed6
Adam Haile <adam.haile@gmail.com>
2018-09-01 17:13:28 -0400

    tsu.AssociativeArray -> IndexedMap for better perf, no tsu dependency

--- es/maptype.js
+++ es/maptype.js
@@ -5,7 +5,147 @@
 |
 | The full license is in the file COPYING.txt, distributed with this software.
 |----------------------------------------------------------------------------*/
-import * as tsu from "../thirdparty/tsu";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 export function createMap(compare) {
-    return new tsu.AssociativeArray(compare);
+    return new IndexedMap();
 }
+var IndexedMap = /** @class */ (function () {
+    function IndexedMap() {
+        this._index = {};
+        this._array = [];
+    }
+    /**
+     * Returns the number of items in the array.
+     */
+    IndexedMap.prototype.size = function () {
+        return this._array.length;
+    };
+    /**
+    * Returns true if the array is empty.
+    */
+    IndexedMap.prototype.empty = function () {
+        return this._array.length === 0;
+    };
+    /**
+     * Returns the item at the given array index.
+     *
+     * @param index The integer index of the desired item.
+     */
+    IndexedMap.prototype.itemAt = function (index) {
+        return this._array[index];
+    };
+    /**
+    * Returns true if the key is in the array, false otherwise.
+    *
+    * @param key The key to locate in the array.
+    */
+    IndexedMap.prototype.contains = function (key) {
+        return this._index[key.id()] !== undefined;
+    };
+    /**
+     * Returns the pair associated with the given key, or undefined.
+     *
+     * @param key The key to locate in the array.
+     */
+    IndexedMap.prototype.find = function (key) {
+        var i = this._index[key.id()];
+        return i === undefined ? undefined : this._array[i];
+    };
+    /**
+     * Returns the pair associated with the key if it exists.
+     *
+     * If the key does not exist, a new pair will be created and
+     * inserted using the value created by the given factory.
+     *
+     * @param key The key to locate in the array.
+     * @param factory The function which creates the default value.
+     */
+    IndexedMap.prototype.setDefault = function (key, factory) {
+        var i = this._index[key.id()];
+        if (i === undefined) {
+            var pair = new Pair(key, factory());
+            this._index[key.id()] = this._array.length;
+            this._array.push(pair);
+            return pair;
+        }
+        else {
+            return this._array[i];
+        }
+    };
+    /**
+     * Insert the pair into the array and return the pair.
+     *
+     * This will overwrite any existing entry in the array.
+     *
+     * @param key The key portion of the pair.
+     * @param value The value portion of the pair.
+     */
+    IndexedMap.prototype.insert = function (key, value) {
+        var pair = new Pair(key, value), i = this._index[key.id()];
+        if (i === undefined) {
+            this._index[key.id()] = this._array.length;
+            this._array.push(pair);
+        }
+        else {
+            this._array[i] = pair;
+        }
+        return pair;
+    };
+    /**
+     * Removes and returns the pair for the given key, or undefined.
+     *
+     * @param key The key to remove from the map.
+     */
+    IndexedMap.prototype.erase = function (key) {
+        var i = this._index[key.id()];
+        if (i === undefined)
+            return undefined;
+        this._index[key.id()] = undefined;
+        var pair = this._array[i], last = this._array.pop();
+        if (pair !== last) {
+            this._array[i] = last;
+            this._index[last.first.id()] = i;
+        }
+        return pair;
+    };
+    /**
+     * Create a copy of this associative array.
+     */
+    IndexedMap.prototype.copy = function () {
+        var copy = new IndexedMap();
+        copy._index = __assign({}, this._index);
+        copy._array = this._array.map(function (p) { return p.copy(); });
+        return copy;
+    };
+    return IndexedMap;
+}());
+/**
+* A class which defines a generic pair object.
+*/
+var Pair = /** @class */ (function () {
+    /**
+    * Construct a new Pair object.
+    *
+    * @param first The first item of the pair.
+    * @param second The second item of the pair.
+    */
+    function Pair(first, second) {
+        this.first = first;
+        this.second = second;
+    }
+    /**
+    * Create a copy of the pair.
+    */
+    Pair.prototype.copy = function () { return new Pair(this.first, this.second); };
+    return Pair;
+}());
--- src/maptype.ts
+++ src/maptype.ts
@@ -6,12 +6,143 @@
 | The full license is in the file COPYING.txt, distributed with this software.
 |----------------------------------------------------------------------------*/
 
-import * as tsu from "../thirdparty/tsu";
+export interface IMap<T extends { id(): number }, U> extends IndexedMap<T, U> { }
 
 export
-interface IMap<T, U> extends tsu.AssociativeArray<T, U> { }
+function createMap<T extends { id() : number }, U>( compare: any ): IMap<T, U> {
+    return new IndexedMap<T, U>();
+}
 
-export
-function createMap<T, U>( compare: tsu.ICompare<T, T> ): IMap<T, U> {
-    return new tsu.AssociativeArray<T, U>( compare );
+class IndexedMap<T extends { id() : number }, U> {
+    public _index = {} as { [ id : number ] : number | undefined };
+    public _array = [] as Pair<T, U>[];
+    
+    /**
+     * Returns the number of items in the array.
+     */
+    public size(): number {
+        return this._array.length;
+    }
+
+    /**
+    * Returns true if the array is empty.
+    */
+    public empty(): boolean {
+        return this._array.length === 0;
+    }
+    
+    /**
+     * Returns the item at the given array index.
+     *
+     * @param index The integer index of the desired item.
+     */
+    public itemAt(index: number): Pair<T, U> {
+        return this._array[index];
+    }
+
+    /**
+    * Returns true if the key is in the array, false otherwise.
+    *
+    * @param key The key to locate in the array.
+    */
+    public contains(key: T) {
+        return this._index[key.id()] !== undefined;
+    }
+
+    /**
+     * Returns the pair associated with the given key, or undefined.
+     *
+     * @param key The key to locate in the array.
+     */
+    public find(key: T) {
+        const i = this._index[key.id()];
+        return i === undefined ? undefined : this._array[i];
+    }
+
+    /**
+     * Returns the pair associated with the key if it exists.
+     *
+     * If the key does not exist, a new pair will be created and
+     * inserted using the value created by the given factory.
+     *
+     * @param key The key to locate in the array.
+     * @param factory The function which creates the default value.
+     */
+    public setDefault(key: T, factory: () => U): Pair<T, U> {
+        const i = this._index[key.id()];
+        if (i === undefined) {
+            const pair = new Pair(key, factory());
+            this._index[key.id()] = this._array.length;
+            this._array.push(pair);
+            return pair;
+        } else {
+            return this._array[i];
+        }
+    }
+
+    /**
+     * Insert the pair into the array and return the pair.
+     *
+     * This will overwrite any existing entry in the array.
+     *
+     * @param key The key portion of the pair.
+     * @param value The value portion of the pair.
+     */
+    public insert(key: T, value: U): Pair<T, U> {
+        const pair = new Pair(key, value),
+            i = this._index[key.id()];
+        if (i === undefined) {
+            this._index[key.id()] = this._array.length;
+            this._array.push(pair);
+        } else {
+            this._array[i] = pair;
+        }
+        return pair;
+    }
+
+    /**
+     * Removes and returns the pair for the given key, or undefined.
+     *
+     * @param key The key to remove from the map.
+     */
+    public erase(key: T): Pair<T, U> {
+        const i = this._index[key.id()];
+        if (i === undefined) return undefined;
+        this._index[key.id()] = undefined;
+        const pair = this._array[i],
+            last = this._array.pop();
+        if (pair !== last) {
+            this._array[i] = last;
+            this._index[last.first.id()] = i;
+        }
+        return pair;
+    }
+
+    /**
+     * Create a copy of this associative array.
+     */
+    public copy(): IndexedMap<T, U> {
+        const copy = new IndexedMap<T, U>();
+        copy._index = { ...this._index };
+        copy._array = this._array.map(p => p.copy());
+        return copy;
+    }
+}
+
+/**
+* A class which defines a generic pair object.
+*/
+class Pair<T, U> {
+    /**
+    * Construct a new Pair object.
+    *
+    * @param first The first item of the pair.
+    * @param second The second item of the pair.
+    */
+    constructor(public first: T, public second: U) { }
+
+    /**
+    * Create a copy of the pair.
+    */
+    public copy() { return new Pair(this.first, this.second); }
 }
